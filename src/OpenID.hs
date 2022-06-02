63c5e14c0bf08c3944e16e888fd2a5e7ff76fa88
iKarsk <krystian.karski@outlook.com>
2022-06-02 23:13:57 +0100

    Add dependencies for OpenID

--- src/OpenID.hs
+++ src/OpenID.hs
@@ -1,1 +1,36 @@
+{-# LANGUAGE DataKinds             #-}
+{-# LANGUAGE DeriveGeneric         #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE FlexibleContexts      #-}
+{-# LANGUAGE FlexibleInstances     #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE NoImplicitPrelude     #-}
+{-# LANGUAGE OverloadedLists       #-}
+{-# LANGUAGE OverloadedStrings     #-}
+{-# LANGUAGE PartialTypeSignatures #-}
+{-# LANGUAGE PolyKinds             #-}
+{-# LANGUAGE RankNTypes            #-}
+{-# LANGUAGE RecordWildCards       #-}
+{-# LANGUAGE ScopedTypeVariables   #-}
+{-# LANGUAGE TypeFamilies          #-}
+{-# LANGUAGE TypeOperators         #-}
+{-# LANGUAGE TypeSynonymInstances  #-}
+
+
 module OpenID where
+
+
+import Protolude
+
+import Data.Aeson (FromJSON (..), (.:))
+import qualified Data.Aeson as JSON
+import qualified Data.Aeson.Types as AeT
+import qualified Data.ByteString.Lazy as LBS
+import qualified Data.List as List
+import qualified Data.Text as Text
+import Jose.Jwt (Jwt (..), decodeClaims)
+import Network.HTTP.Client (Manager, newManager)
+import Network.HTTP.Client.TLS (tlsManagerSettings)
+import Network.Wai.Handler.Warp (run)
+import Servant
+import Servant.HTML.Blaze (HTML)
