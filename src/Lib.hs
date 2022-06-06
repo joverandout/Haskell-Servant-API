e132242c17e86737174653bc3266f966f3674948
joverandout <joverandout@gmail.com>
2022-06-06 09:55:58 +0100

    combined created new user functions using the pure operator

--- src/Lib.hs
+++ src/Lib.hs
@@ -100,8 +100,11 @@ testUsers :: [User]
 testUsers = [isaac, albert, joe]
 
 
-createNewUser :: String -> String -> Int -> String -> IO()
-createNewUser name email age occupation = addUserToDB (User name email age occupation)
+createNewUser :: String -> String -> Int -> String -> IO User
+createNewUser name email age occupation = do
+  let user = User name email age occupation
+  addUserToDB user
+  pure user
 
 -- static web filepath
 www :: FilePath
